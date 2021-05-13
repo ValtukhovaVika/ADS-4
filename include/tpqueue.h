accd035a628eb75a9bc5685f90a717aa4bb6ed5d
ValtukhovaVika <75724011+ValtukhovaVika@users.noreply.github.com>
2021-05-13 23:43:19 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,17 +5,17 @@
 
 template<typename T>
 class TPQueue {
-  private:
+ private:
     T *arr;
     int size;
     int begin,
         end;
     int count;
-public:
+ public:
     TPQueue(int = 100);
     ~TPQueue();
- 
-    void push(const T &);
+    
+  void push(const T &);
     T pop();
     T get() const;
     bool isEmpty() const;
@@ -23,15 +23,13 @@ public:
 };
 
 template<typename T>
-TPQueue<T>::TPQueue : size(100),
-    begin(0), end(0), count(0)
-{
+TPQueue<T>::TPQueue() : size(100),
+    begin(0), end(0), count(0){
     arr = new T[size + 1];
 }
 
 template<typename T>
-TPQueue<T>::~TPQueue()
-{
+TPQueue<T>::~TPQueue() {
     delete [] arr;
 }
 
@@ -63,37 +61,31 @@ void TPQueue<T>::push(const T& item) {
 }
 
 template<typename T>
-T TPQueue<T>::pop()
-{
-    assert( count > 0 );
+T TPQueue<T>::pop() {
+    assert(count > 0);
  
     T item = arr[begin++];
     count--;
- 
-    if (begin > size)
+  if (begin > size)
         begin -= size + 1;
- 
+  
     return item;
 }
 
-
 template<typename T>
-T TPQueue<T>::get() const
-{
-    assert( count > 0 );
+T TPQueue<T>::get() const {
+    assert(count > 0);
     return arr[begin];
 }
 
 template<typename T>
-bool TPQueue<T>::isEmpty() const
-{
-  return count==0;
+bool TPQueue<T>::isEmpty() const {
+  return count == 0;
 }
 
 template<typename T>
-bool TPQueue<T>::isFull() const
-{
-  return count==size;
+bool TPQueue<T>::isFull() const {
+  return count == size;
 }
 
 struct SYM {
